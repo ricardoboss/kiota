ca342a0726bc2adaeab05d671f8769a507c129e8
Ricardo Boss <mail@ricardoboss.de>
2025-03-14 22:29:21 +0100

    Better logging

--- Kiota.Web/Program.cs
+++ Kiota.Web/Program.cs
@@ -6,11 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddLogging(c =>
-{
-    c.ClearProviders();
-    c.AddProvider(new WebConsoleLoggerProvider());
-});
+builder.Services.AddLogging();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
